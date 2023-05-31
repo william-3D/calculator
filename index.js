66d24d34cb814f450fd04eec13bdc35f85a53787
Sonia William <williamlaw2017@outlook.com>
2023-05-31 19:29:48 +0800

    allow multiple digits to be displayed

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const buttons = document.querySelector(".buttons").children;
 
 for (let button of buttons) {
   button.addEventListener("click", () => {
-    display.innerText = button.innerText;
+    display.innerText += button.innerText;
   });
 }
