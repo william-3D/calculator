256df5e91aad4be7e5bea0f122da496de9125522
Sonia William <williamlaw2017@outlook.com>
2023-05-31 19:49:44 +0800

    declare more variables

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelector(".buttons").children;
+const allSymbols = ["+", "-", "X", "/", "%", "AC", "="];
+
+let firstValue = "";
+let secondValue = "";
+let symbol = "";
 
 for (let button of buttons) {
   button.addEventListener("click", () => {
     const { innerText: btnValue } = button;
     display.innerText += btnValue;
+    const btnValueIsSymbol = allSymbols.includes(btnValue);
   });
 }
